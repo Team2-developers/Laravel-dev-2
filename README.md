40bea3ec2b69493d8d0e8733f90b65d778fb521e
miyagawa <2101297@s.asojuku.ac.jp>
2023-07-26 22:42:32 +0900

    imgテーブルのシーダーの作成

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Laravel-dev-2
+
+
+composer install
+docker-compsoe up -d --build
+
+cd laravel-project
+
+php artisan migrate
+php artisan migrate:rollback
+php artisan db:seed --class=ImagesTableSeeder
+
---
+++ laravel-project/database/seeders/ImagesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $imgPaths = [
+            'https://mp-class.chips.jp/lifegame/cat_user1.jpg',
+            'https://mp-class.chips.jp/lifegame/cat_user2.jpg',
+            'https://mp-class.chips.jp/lifegame/cat_user3.jpg',
+            'https://mp-class.chips.jp/lifegame/cat_user4.jpg',
+        ];
+
+        foreach ($imgPaths as $imgPath) {
+            DB::table('imgs')->insert([
+                'img_pass' => $imgPath,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
